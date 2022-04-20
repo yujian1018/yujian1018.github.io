8d2d13fb11cbbfebf306c9ce80e7313d448ee224
yujian <yujian@htjicon.com>
2022-04-20 10:32:26 +0800

    调整url路径

--- book/linux/2.shell-教程/2.2.-shell脚本/cron.monthly.sh
+++ book/linux/2.shell-教程/2.2.-shell脚本/cron.monthly.sh
@@ -12,7 +12,7 @@ date: 2019-08-30T15:13:01+08:00
 DATE=$(date +%F_%H_%M)
 
 fun_nginx() {
-    #nginx按天记录日志
+    #nginx按月记录日志
     mv /usr/local/nginx/logs/access.log /usr/local/nginx/logs/access_${DATE}.log
     /usr/local/nginx/sbin/nginx -s reload
 }
