284c23a1ade1adac5a7f81a95f78e2f090d6983a
yujian <yujian@htjicon.com>
2022-08-25 10:50:37 +0800

    add /nav

---
+++ nav/js/configs.js
@@ -0,0 +1,48 @@
+var configs = [
+    {
+        "tabs": [],
+        "items": [
+            [
+                { url: "https://pro.oss.01cs.cc", "icon": "image/oss.png", "title": "oss", "desc": "pro" },
+            ],
+            [
+                { url: "https://prepro.oss.01cs.cc", "icon": "image/oss.png", "title": "oss", "desc": "prepro" },
+            ],
+            [
+                { url: "https://test.oss.01cs.cc", "icon": "image/oss.png", "title": "oss", "desc": "test" },
+            ],
+            [
+                { url: "https://dev.oss.01cs.cc", "icon": "image/oss.png", "title": "oss", "desc": "dev" },
+            ]]
+    },
+    {
+        "tabs": [],
+        "items": [
+            [
+                { url: "#", icon: "image/yapi.png", title: "AI接口", desc: "NLP" },
+            ],
+            [
+                { url: "http://119.3.45.6/api/api_1.html", icon: "image/yapi.png", title: "AI接口", desc: "NLP" },
+                { url: "http://119.3.45.6/api2/api_3.html", icon: "image/yapi.png", title: "数据接口", desc: "查询业务数据" },
+            ]
+        ]
+    }, {
+        "tabs": [],
+        "items": [
+            [
+                { url: "https://git.01cs.cc/", icon: "image/git.png", title: "git地址", desc: "工具" },
+            ],
+            [
+                { url: "#", icon: "image/oil-help.png", title: "注册", desc: "" },
+            ],
+            [
+                { url: "#", icon: "image/user.png", title: "用户注册协议和隐私政策", desc: "《用户注册协议和隐私政策》" },
+            ],
+            [
+                { url: "#", icon: "image/weixin.png", title: "下载", desc: "" },
+            ]
+        ]
+    }
+];
+
+
