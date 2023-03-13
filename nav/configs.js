8b6affdb57a261a01bc1ef18dede42beb96d6c51
yujian <yujian1018@gmail.com>
2023-03-13 12:15:46 +0800

    导航页面

---
+++ nav/configs.js
@@ -0,0 +1,76 @@
+var configs = [
+    {
+        "nav_name": "内部项目导航",
+        "items": [
+            {
+                "title": "好问角后台管理系统",
+                "notice": "好问角后台管理系统",
+                "icon": "haowenjiao.png",
+                "links": [
+                    { "link_name": "演示环境", "url": "https://gm.qingxuai.com/" },
+                    {},
+                    {},
+                    {}
+                ]
+            },
+            {
+                "title": "长三角推送系统",
+                "notice": "长三角推送系统",
+                "icon": "procurement.png",
+                "links": [
+                    { "link_name": "设计", "url": "https://lanhuapp.com/link/#/invite?sid=lX0hXgYQ" },
+                    { "link_name": "测试环境", "url": "https://procurement.qingxuai.com/html/" },
+                    { "link_name": "演示环境", "url": "https://procurement.qingxuai.com/html/" },
+                    { "link_name": "正式环境", "url": "https://procurement.qingxuai.com/html/" },
+                    { "link_name": "API接口", "url": "https://procurement.qingxuai.com/html/" },
+                ]
+            },
+            {
+                "title": "爬虫引擎",
+                "notice": "系统",
+                "icon": "spider.png",
+                "links": [
+                    { "link_name": "演示环境", "url": "https://spider.qingxuai.com/" },
+                    {},
+                    {},
+                    {}
+                ]
+            },
+        ]
+    },
+    {
+        "nav_name": "工具",
+        "items": [
+            {
+                "title": "gitea",
+                "notice": "内部代码仓库",
+                "icon": "git.png",
+                "links": [
+                    { "link_name": "", "url": "https://git.qingxuai.com/" },
+                ]
+            },
+            {
+                "title": "禅道",
+                "notice": "项目管理系统",
+                "icon": "chandao.png",
+                "links": [
+                    { "link_name": "", "url": "https://zbox.qingxuai.com/" },
+                ]
+            },
+            {
+                "title": "minio",
+                "notice": "文件存储",
+                "icon": "oss.png",
+                "links": [
+                    { "link_name": "开发环境", "url": "https://ossadmin.qingxuai.com/" },
+                    { "link_name": "测试环境", "url": "https://ossadmin.qingxuai.com/" },
+                    { "link_name": "演示环境", "url": "https://ossadmin.qingxuai.com/" },
+                    { "link_name": "正式环境", "url": "https://ossadmin.qingxuai.com/" },
+                    { "link_name": "官网", "url": "https://min.io" },
+                ]
+            },
+        ]
+    }
+];
+
+
