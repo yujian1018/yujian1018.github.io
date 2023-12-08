e5a42801ac150643b2e6ec2336bf8f82942b4a35
yujian <yujian1018@gmail.com>
2023-12-08 11:34:21 +0800

    导航页更新

--- nav/configs.js
+++ nav/configs.js
@@ -2,59 +2,13 @@ var configs = [
     {
         "nav_name": "内部项目导航",
         "items": [
+
             {
-                "title": "好问角后台管理系统",
-                "notice": "好问角后台管理系统",
-                "icon": "haowenjiao.png",
-                "links": [
-                    { "link_name": "演示环境", "url": "https://gm.qingxuai.com/" },
-                    {},
-                    {},
-                    {}
-                ]
-            },
-            {
-                "title": "长三角推送系统",
-                "notice": "长三角推送系统",
-                "icon": "procurement.png",
-                "links": [
-                    { "link_name": "设计", "url": "https://lanhuapp.com/link/#/invite?sid=lX0hXgYQ" },
-                    { "link_name": "测试环境", "url": "https://procurement.qingxuai.com/html/" },
-                    { "link_name": "演示环境", "url": "https://procurement.qingxuai.com/html/" },
-                    { "link_name": "正式环境", "url": "https://procurement.qingxuai.com/html/" },
-                    { "link_name": "API接口", "url": "https://procurement.qingxuai.com/html/" },
-                ]
-            },
-            {
-                "title": "爬虫引擎",
-                "notice": "系统",
-                "icon": "spider.png",
-                "links": [
-                    { "link_name": "演示环境", "url": "https://spider.qingxuai.com/" },
-                    {},
-                    {},
-                    {}
-                ]
-            },
-            {
-                "title": "印路科技销售工具",
-                "notice": "系统 road",
+                "title": "印路科技",
+                "notice": "印路项目",
                 "icon": "maintenance.png",
                 "links": [
-                    { "link_name": "产品原型图", "url": "https://9f4v38.axshare.com/#id=qa9s4k&p=%E7%99%BB%E5%BD%95%E9%A1%B5" },
-                    { "link_name": "cloudflow", "url": "http://hybridsoftware.net.cn:9090/" },
-                    { "link_name": "测试环境", "url": "https://hybrid.qingxuai.com/" },
-                    {}
-                ]
-            },
-            {
-                "title": "官网",
-                "notice": "官方站点",
-                "icon": "maintenance.png",
-                "links": [
-                    { "link_name": "www1", "url": "https://www1.qingxuai.com" },
-                    { "link_name": "www2", "url": "https://www2.qingxuai.com" },
-                    {}
+                    { "link_name": "导航页", "url": "https://oss.qingxuai.com/website/nav.qingxuai.com/index.html" },
                 ]
             },
         ]
@@ -83,15 +37,58 @@ var configs = [
                 "notice": "文件存储",
                 "icon": "oss.png",
                 "links": [
-                    { "link_name": "开发环境", "url": "https://ossadmin.qingxuai.com/" },
                     { "link_name": "测试环境", "url": "https://ossadmin.qingxuai.com/" },
-                    { "link_name": "演示环境", "url": "https://ossadmin.qingxuai.com/" },
                     { "link_name": "正式环境", "url": "https://ossadmin.qingxuai.com/" },
                     { "link_name": "官网", "url": "https://min.io" },
                 ]
             },
         ]
-    }
+    },
+    {
+        "nav_name": "2023",
+        "items": [
+            {
+                "title": "官网",
+                "notice": "官方站点",
+                "icon": "maintenance.png",
+                "links": [
+                    { "link_name": "速软动力", "url": "https://www1.qingxuai.com" },
+                    { "link_name": "印路科技", "url": "https://www2.qingxuai.com" },
+                ]
+            },
+        ]
+    },
+    {
+        "nav_name": "2022",
+        "items": [
+            {
+                "title": "好问角后台管理系统",
+                "notice": "好问角后台管理系统",
+                "icon": "haowenjiao.png",
+                "links": [
+                    { "link_name": "演示环境", "url": "https://gm.qingxuai.com/" },
+                ]
+            },
+            {
+                "title": "长三角推送系统",
+                "notice": "长三角推送系统",
+                "icon": "procurement.png",
+                "links": [
+                    { "link_name": "设计", "url": "https://lanhuapp.com/link/#/invite?sid=lX0hXgYQ" },
+                    { "link_name": "正式环境", "url": "https://procurement.qingxuai.com/html/" },
+                    { "link_name": "API接口", "url": "https://procurement.qingxuai.com/html/" },
+                ]
+            },
+            {
+                "title": "爬虫引擎",
+                "notice": "系统",
+                "icon": "spider.png",
+                "links": [
+                    { "link_name": "演示环境", "url": "https://spider.qingxuai.com/" },
+                ]
+            },
+        ]
+    },
 ];
 
 
